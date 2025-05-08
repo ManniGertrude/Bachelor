73f80a632a26cce3bd5d96b3422446ddfa3fdeff
ManniGertrude <mannigertrude@gmail.com>
2025-05-08 21:22:21 +0200

    deleted:    Arduino Testrun/analyse_voltage.pdf 	modified:   Arduino Testrun/arduino_power_plot.py 	renamed:    Arduino Testrun/data/001/VN1/VN_0_krad_chip001_20250508_153648 copy 2.csv -> Arduino Testrun/data/001/VN1/VN_0_krad_chip001_20250508_153646.csv 	renamed:    Arduino Testrun/data/001/VN3/VN_0_krad_chip001_20250508_153648.csv -> Arduino Testrun/data/001/VN2/VN_0_krad_chip001_20250508_153648.csv 	renamed:    Arduino Testrun/data/001/VN2/VN_0_krad_chip001_20250508_153648 copy.csv -> Arduino Testrun/data/001/VN3/VN_0_krad_chip001_20250508_153647.csv 	deleted:    Arduino Testrun/data/001/VN3/VN_0_krad_chip001_20250508_153648 copy 2.csv 	renamed:    Arduino Testrun/data/001/VN3/VN_0_krad_chip001_20250508_153648 copy.csv -> Arduino Testrun/data/001/VN3/VN_0_krad_chip001_20250508_153649.csv 	new file:   Arduino Testrun/data/20250508_153649.pdf 	deleted:    Arduino Testrun/data/arduino_power_plot.pdf 	new file:   Arduino Testrun/data/latest.pdf

--- Arduino Testrun/arduino_power_plot.py
+++ Arduino Testrun/arduino_power_plot.py
@@ -1,20 +1,26 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+import shutil
 from matplotlib.backends.backend_pdf import PdfPages
+
 
 
 pathtest = f'{os.path.dirname(__file__)}/data'
 
 
-def arduino_power_plot(path):
-    pdf_path = f'{path}/arduino_power_plot.pdf'
+def arduino_power_plot(path, output_path, Temp, HV):
+    pdf_path = f'{output_path}/latest.pdf'
     with PdfPages(pdf_path) as pdf:
+        timestamps = []
         for root, dirs, files in os.walk(path):
             csv_files = [file for file in files if file.endswith('.csv')]
             if len(csv_files) > 0:
-                csv_files.sort(key=lambda f: os.path.getmtime(os.path.join(root, f)), reverse=True)
+                # Sort files by timestamp
+                csv_files.sort(reverse=True)
+                timestamps.append(csv_files[0][-19:-4])
                 data_file = csv_files[0]
+                
                 fig, ax1 = plt.subplots()
                 ax2 = ax1.twinx()
                 
@@ -22,22 +28,23 @@ def arduino_power_plot(path):
                 ax2.scatter(df['DAC_Value_lit'], 1000*df['vdda_curr'], color = 'navy', label='vdda current', marker='^', alpha=0.8)
                 ax2.scatter(df['DAC_Value_lit'], 1000*df['vddd_curr'], color = 'slateblue', label='vddd current', marker='p', alpha=0.8)    
                 ax1.scatter(df['DAC_Value_lit'], df['DAC_Value_exp'], label=f'{df["dac_name"][0]} voltage', marker='X', color='r', alpha=0.8)
+                
                 handles1, labels1 = ax1.get_legend_handles_labels()
                 handles2, labels2 = ax2.get_legend_handles_labels()
                 handles = handles1 + handles2
                 labels = labels1 + labels2
                 
-
                 ax1.set_xlabel('set DAC voltage [ ]', fontsize=12)
                 ax2.set_ylabel('supplied current [mA]', color = 'navy', fontsize = 12)
                 ax1.set_ylabel('real DAC voltage [ ]', color = 'r', fontsize=12)
                 ax1.tick_params(axis='y', labelcolor='r')
                 ax2.tick_params(axis='y', labelcolor='navy')
                 ax1.legend(handles, labels)
+                plt.legend([f'temp = {Temp}°C', f'HV = {HV}V'], loc='lower right', markerscale = 0, framealpha=0.5)
                 ax1.grid()
-                plt.legend(['Temp = 30 °C', 'HV = 5V'], loc='lower right', markerscale = 0, framealpha=0.5)
                 pdf.savefig(fig, bbox_inches='tight')
                 plt.close(fig)
-    print(f"Plots saved to {pdf_path}")
+    timestamps.sort(reverse=True)
+    shutil.copy(pdf_path, f'{output_path}/{timestamps[0]}.pdf')
 
-arduino_power_plot(pathtest)
+arduino_power_plot(pathtest, pathtest, 1, 1)
