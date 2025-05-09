4b9544fccd814d931420ddfdd3ed2183e4c44e5a
ManniGertrude <mannigertrude@gmail.com>
2025-05-09 11:51:30 +0200

    modified:   Arduino Testrun/arduino_power_plot.py 	modified:   Arduino Testrun/data/20250508_153649.pdf 	modified:   Arduino Testrun/data/latest.pdf

--- Arduino Testrun/arduino_power_plot.py
+++ Arduino Testrun/arduino_power_plot.py
@@ -10,6 +10,8 @@ pathtest = f'{os.path.dirname(__file__)}/data'
 
 
 def arduino_power_plot(path, output_path, infostring=None):
+    TEXT_COLOR = '#07529a'
+    TITLE_COLOR = '#07529a'
     pdf_path = f'{output_path}/latest.pdf'
     with PdfPages(pdf_path) as pdf:
         timestamps = []
@@ -24,27 +26,35 @@ def arduino_power_plot(path, output_path, infostring=None):
                 ax2 = ax1.twinx()
                 
                 df = pd.read_csv(f'{root}/{data_file}')
-                ax2.scatter(df['DAC_Value_lit'], 1000*df['vdda_curr'], color = 'navy', label='vdda current', marker='^', alpha=0.8)
-                ax2.scatter(df['DAC_Value_lit'], 1000*df['vddd_curr'], color = 'slateblue', label='vddd current', marker='p', alpha=0.8)    
-                ax1.scatter(df['DAC_Value_lit'], df['DAC_Value_exp'], color = 'crimson', label='real DAC voltage', marker='o', alpha=0.8)
+                ax2.plot(df['DAC_Value_lit'].values, 1000*df['vdda_curr'].values, color = 'navy', label='vdda current', marker='^', alpha=0.8)
+                ax2.plot(df['DAC_Value_lit'].values, 1000*df['vddd_curr'].values, color = 'slateblue', label='vddd current', marker='p', alpha=0.8)    
+                ax1.plot(df['DAC_Value_lit'].values, df['DAC_Value_exp'].values*5/4.735, color = 'crimson', label='real DAC voltage', marker='o', alpha=0.8)
+                
                 handles1, labels1 = ax1.get_legend_handles_labels()
                 handles2, labels2 = ax2.get_legend_handles_labels()
                 handles = handles1 + handles2
                 labels = labels1 + labels2
                 
-                ax1.set_xlabel('set DAC voltage [ ]', fontsize=12)
-                ax2.set_ylabel('supplied current [mA]', color = 'navy', fontsize = 12)
-                ax1.set_ylabel('real DAC voltage [ ]', color = 'crimson', fontsize=12)
+                ax1.set_title(f'DAC value analysis and powermonitoring', color = TITLE_COLOR, fontsize=14)
+                ax1.set_xlabel('set DAC voltage / ?', fontsize=12)
+                ax1.set_ylabel('real DAC voltage / V', color = 'crimson', fontsize=12)
+                ax2.set_ylabel('supplied current / mA', color = 'navy', fontsize = 12)
+
                 ax1.tick_params(axis='y', labelcolor='crimson')
                 ax2.tick_params(axis='y', labelcolor='navy')
-                ax1.legend(handles, labels)
-                if infostring != None:
-                    plt.legend(infostring, loc='lower right', markerscale = 0, framealpha=0.5)
+
+                ax1.legend(handles, labels,  title = infostring, loc='upper left')
+                ax1.set_ylim(0, 1.2)
+                
+                # fig.text(0.1, 0.92, "Run2021", fontsize=12, color=TEXT_COLOR)
+                # fig.text(0.7, 0.92, 'bla bla bla', fontsize=12, color=TEXT_COLOR)
+                
                 ax1.grid()
                 pdf.savefig(fig, bbox_inches='tight')
                 plt.close(fig)
+    
     timestamps.sort(reverse=True)
     shutil.copy(pdf_path, f'{output_path}/{timestamps[0]}.pdf')
 
-infostring_t = [f'temp = 5°C', f'HV = -81V']
+infostring_t = f'temp = 5°C \n HV = -81V'
 arduino_power_plot(pathtest, pathtest, infostring_t)
