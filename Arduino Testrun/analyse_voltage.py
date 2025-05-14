0322daa280366761ca695a7fd65ea50051fd540d
ManniGertrude <mannigertrude@gmail.com>
2025-05-14 17:46:08 +0200

    modified:   Arduino Testrun/analyse_voltage.py 	modified:   Arduino Testrun/analyse_voltage_lin.pdf 	modified:   Arduino Testrun/analyse_voltage_log.pdf 	modified:   Arduino Testrun/data/20250508_153649.pdf 	modified:   Arduino Testrun/data/latest.pdf

--- Arduino Testrun/analyse_voltage.py
+++ Arduino Testrun/analyse_voltage.py
@@ -6,7 +6,7 @@ import pandas as pd
 import os
 
 def lin(Params, x):
-    return Params[0] * x + Params[1]
+    return Params[0] * np.array(x) + Params[1]
 
 plt.subplot()
 
@@ -36,28 +36,33 @@ for column in data.columns:
     # print([column]*16, data[column].values)
     
     # plots the data
-    datax.append([column]*16)
-    datay.append(data[column].values*4.737/5)
-    datayerr.append(np.std(data[column].values)*4.737/5)
+    datax.extend([column]*16) 
+    datay.extend(data[column].values*4.737/5)
+    datayerr.extend([np.std(data[column].values) * 4.737 / 5] * 16)
     plt.errorbar([column]*16, data[column].values*4.737/5, xerr=0.0005*column + 0.005, yerr=np.std(data[column].values)*4.737/5 , capsize=3, linestyle='None',  fmt='.', label=f'{column} V')
 
 lin_values = np.linspace(0, 5, 10000)
 odr_model = odr.Model(lin)
-odr_data = odr.RealData(datax, datay, sx=0.0005*np.array(datax) + 0.005, sy=np.std(datay)*np.ones(len(datax)))
+odr_data = odr.RealData(datax, datay, sx=0.0005*np.array(datax) + 0.005, sy=datayerr)
 odr_fit = odr.ODR(odr_data, odr_model, beta0=[1., 0.])
 odr_out = odr_fit.run()
-print(odr_out.beta, odr_out.sd_beta)
+print(odr_out.beta)
+print(odr_out.sd_beta)
 
-chi2 = np.sum(((lin(odr_out.beta, datax) - datay) / datayerr) ** 2)
+chi2 = sum((datay - lin(odr_out.beta, datax))**2 / np.power(datayerr, 2))	
+chi2_red = chi2 / (len(datay) - len(odr_out.beta))
+print(f'chi2_red: {chi2:.3f}')
+print(f'chi2_red: {chi2_red:.3f}')
 
-plt.legend()
-plt.plot(lin_values, lin_values, label='Theorie', color='red')
-plt.plot(lin_values, lin(odr_out.beta, lin_values), label='fittet data', color='black', linestyle='--', alpha=0.8)
-plt.xlabel('set value [V]', fontsize=12)
-plt.ylabel('measured value [V]', fontsize=12)
+plt.plot(lin_values, lin(odr_out.beta, lin_values), label='fittet data: $\chi^2_{red} = $' + f'{chi2_red:.3f}', color='black', linestyle='--', alpha=0.8)
+plt.legend(framealpha = 0, fontsize=10, loc='upper left')
+plt.title('Arduino voltage verification', fontsize=14)
+plt.xlabel('voltage_measured / V', fontsize=12)
+plt.ylabel('voltage_set / V', fontsize=12)
 plt.grid()
 plt.savefig(f'{path}/analyse_voltage_lin.pdf')
 plt.xscale('log')
-plt.grid()
-plt.savefig('ArdiTesti/analyse_voltage.pdf')
+plt.yscale('log')
+plt.grid(True)
+plt.savefig(f'{path}/analyse_voltage_log.pdf', bbox_inches='tight')
 plt.show
