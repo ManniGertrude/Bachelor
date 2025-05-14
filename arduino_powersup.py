132d871c9951cb645895bbd9a3a9049dc1c4fecc
ManniGertrude <mannigertrude@gmail.com>
2025-05-14 17:19:14 +0200

    modified:   arduino_powersup.py 	modified:   output_data/tid/Max/DAC_comparison.pdf

--- arduino_powersup.py
+++ arduino_powersup.py
@@ -158,6 +158,7 @@ def DAC_comparison_plot(path, infostring=None, save = True):
     TEXT_COLOR = '#07529a'
     TITLE_COLOR = '#07529a'
     pdf_path = f'{path}/DAC_comparison.pdf'
+    counter = 0
     with PdfPages(pdf_path) as pdf:
         for root, dirs, files in os.walk(path):
             if root[-9:] == "dac_power":
@@ -165,37 +166,37 @@ def DAC_comparison_plot(path, infostring=None, save = True):
                 for lower_root, lower_dirs, lower_files in os.walk(root):
                     csv_files = [file for file in lower_files if file.endswith('.csv')]
                     
-                    if len(csv_files) != 0:
+                    if len(csv_files) != 0 and counter < 10:
+                        counter += 1
                         df = pd.read_csv(f'{lower_root}/{csv_files[-1]}', skipinitialspace=True)
+                        print(f"Processing file: {lower_root}/{csv_files[-1]}")
+                        fig, ax1 = plt.subplots()
+                        ax2 = ax1.twinx()
+
+         
+                        ax2.plot(df['DAC_set_value'].values, 1000*df['vdda_curr'].values, color = 'navy', label='vdda current', marker='o', alpha=0.8)
+                        ax2.plot(df['DAC_set_value'].values, 1000*df['vddd_curr'].values, color = 'slateblue', label='vddd current', marker='o', alpha=0.8)    
+                        ax1.plot(df['DAC_set_value'].values, df['DAC_measured'].values*5/4.735, color = 'crimson', label='DAC_measured', marker='o', alpha=0.8)
                         
-                fig, ax1 = plt.subplots()
-                ax2 = ax1.twinx()
+                        handles1, labels1 = ax1.get_legend_handles_labels()
+                        handles2, labels2 = ax2.get_legend_handles_labels()
+                        handles = handles1 + handles2
+                        labels = labels1 + labels2
+                        
+                        ax1.set_title(f'DAC analysis: {df["DAC_name"][0]}', color = TITLE_COLOR, fontsize=14)
+                        ax1.set_xlabel('DAC_value / DAC', fontsize=12)
+                        ax1.set_ylabel('DAC_measured / V', color = 'crimson', fontsize=12)
+                        ax2.set_ylabel('current / mA', color = 'navy', fontsize = 12)
 
+                        ax1.tick_params(axis='y', labelcolor='crimson')
+                        ax2.tick_params(axis='y', labelcolor='navy')
 
-                df = pd.read_csv(f'{root}/{data_file}', skipinitialspace=True)
-                ax2.plot(df['DAC_set_value'].values, 1000*df['vdda_curr'].values, color = 'navy', label='vdda current', marker='o', alpha=0.8)
-                ax2.plot(df['DAC_set_value'].values, 1000*df['vddd_curr'].values, color = 'slateblue', label='vddd current', marker='o', alpha=0.8)    
-                ax1.plot(df['DAC_set_value'].values, df['DAC_measured'].values*5/4.735, color = 'crimson', label='DAC_measured', marker='o', alpha=0.8)
-                
-                handles1, labels1 = ax1.get_legend_handles_labels()
-                handles2, labels2 = ax2.get_legend_handles_labels()
-                handles = handles1 + handles2
-                labels = labels1 + labels2
-                
-                ax1.set_title(f'DAC analysis: {df["DAC_name"][0]}', color = TITLE_COLOR, fontsize=14)
-                ax1.set_xlabel('DAC_value / DAC', fontsize=12)
-                ax1.set_ylabel('DAC_measured / V', color = 'crimson', fontsize=12)
-                ax2.set_ylabel('current / mA', color = 'navy', fontsize = 12)
-
-                ax1.tick_params(axis='y', labelcolor='crimson')
-                ax2.tick_params(axis='y', labelcolor='navy')
-
-                ax1.legend(handles, labels,  title = infostring)
-                ax1.set_ylim(0, max(df['DAC_measured'].values*5/4.735)*1.05)
-                
-                ax1.grid()
-                pdf.savefig(fig, bbox_inches='tight')
-                plt.close(fig)
+                        ax1.legend(handles, labels,  title = infostring)
+                        ax1.set_ylim(0, max(df['DAC_measured'].values*5/4.735)*1.05)
+                        
+                        ax1.grid()
+                        pdf.savefig(fig, bbox_inches='tight')
+                        plt.close(fig)
 
 try:
     test_path = f'{os.path.dirname(__file__)}/output_data/tid/Max/'
